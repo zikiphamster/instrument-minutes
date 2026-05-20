6aea1b4f722839f0392a84c1bbd9ae5e97ff0ee8
zikiphamster <zacharytpham@gmail.com>
2026-05-20 19:41:13 -0400

    Flame icon changes #2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==================== VERSION ====================
-const APP_VERSION = '1.6.3';
+const APP_VERSION = '1.6.4';
 
 // ==================== CONFIG ====================
 const GIST_ID = 'ab0f0b0a12593cccc0efd7db998410e4';
